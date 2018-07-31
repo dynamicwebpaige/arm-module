29f820a6dcce1d90c7bd83e438de473de72c3fba
Paige Bailey <pabailey@microsoft.com>
2018-07-31 22:39:22 +0000

    Add files via upload

---
+++ market-basket-analysis.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+
+# Import .json file and convert to pandas dataframe
+
+# One-hot encode all file names and subdirs
+
+# Obtain association rules
