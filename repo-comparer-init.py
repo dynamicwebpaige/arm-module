f83b146fedbf66c0c04c64828cdd5bdbf2807765
Paige Bailey <pabailey@microsoft.com>
2018-07-31 22:54:20 +0000

    Add files via upload

--- repo-comparer-init.py
+++ repo-comparer-init.py
@@ -2,8 +2,16 @@ import os
 import json
 
 repos = {}
+filenames = []
+
+# Walk through each one of the directories and build a hashmap of 
+# { dir_name : ['each', 'file', 'in', 'dir']
 
 for i in os.walk('./azure-quickstart-templates').next()[1]:
 	repos[i] = os.listdir('./azure-quickstart-templates/' + str(i))
+	filenames.append(os.listdir('./azure-quickstart-templates/' + str(i)))
 
-print json.dumps(repos)
+# Obtain all unique files in repos
+unique_files = set([item for sublist in filenames for item in sublist])
+
+
